f56ebd60e523dff14c793e0e78733fc498bf6e14
chebishev <atanas.chebishev@gmail.com>
2023-04-11 16:14:59 +0300

    Added camera button with OnClick Listener, added welcome text. Added Permissions check in the manifest

--- app/src/main/java/com/example/asanarecognizer/MainActivity.kt
+++ app/src/main/java/com/example/asanarecognizer/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.asanarecognizer
 
 import android.content.Intent
 import android.hardware.camera2.CameraDevice
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View;
 import android.widget.ActionMenuView
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +24,8 @@ class MainActivity : AppCompatActivity() {
 
         val cameraButton = findViewById<FloatingActionButton>(R.id.camera_button)
         cameraButton.setOnClickListener {
-            println("Clicked")
+           val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
         }
     }
 }
